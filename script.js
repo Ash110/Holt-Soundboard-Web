727861d2dfdbcaa7031a099fee0730106916ee42
Ashwin Alaparthi <ashwin.alaparthi@gmail.com>
2018-12-21 12:25:32 +0530

    `

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function makeEverythingAppear(boxes)
         boxes[i].style.display="block";
 }
 $(document).ready(function() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+      });
+    }
     $(".col-4").addClass("jackInTheBox");
 	var search=0;
 	var mywindow = $(window);
